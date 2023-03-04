8f85cf42ac34ca20df2fa4e9915afd367c1c1f5e
llamaladejam <charliemarcussmith@googlemail.com>
2023-03-04 13:24:22 +0000

    CS: Mounting and fetching written, now we need to deal with Coors error through Django app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,47 @@ class App extends React.Component{      //inherits from React class
     super(props);
     // props = properties - look at notes
     // The state is a built-in React object that is used to contain data or information about the component. 
-    this.state{
+    this.state = {
       todoList:[],  //Array of list items
+      //currently held item list.
+      // Every time we hit edit, we are going to prepopulate this with these values.
       activeItem:{
         id:null,
         title:'',
-        completed:false;
-      }
-
+        completed:false,
+      },
+      //editing: lets us know if we are editing an item or submitting a new one.
+      editing:false,
     }
+    this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+
+  // The componentWillMount() method allows us to execute the React code synchronously 
+  // when the component gets loaded or mounted in the DOM (Document Object Model). 
+  // This method is called during the mounting phase of the React Life-cycle.
+  componentWillMount(){
+    this.fetchTasks()
   }
+
+  fetchTasks(){
+    console.log('Fetching...')
+
+    //use fetch api to mnake api calls
+    fetch('http://127.0.0.1:8000/api/task-list/')       //fetching api url
+    .then(response => response.json())                  //converting response to JSON
+    .then(data => 
+      console.log('Data:', data)
+      )
+
+
+  }
+
+  
 
   render(){                             //Render method
     return(
-      <div classname="container">
+      <div className="container">
 
         <div id="task-container">
           <div id="form-wrapper">
